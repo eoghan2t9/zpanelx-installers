f9404d53bd52714ecde4cac45e24b6b035f46c7c
Eoghan Cunningham <eoghan61@gmail.com>
2014-11-23 20:19:23 +0000

    apt-get fix

--- ubuntu-14.04-LTS-apache2.4.9-php5.5.14.sh
+++ ubuntu-14.04-LTS-apache2.4.9-php5.5.14.sh
@@ -186,7 +186,7 @@ apt-get update
 
 
 # Install some standard utility packages required by the installer and/or ZPX.
-apt-get -y install sudo wget vim make zip unzip git debconf-utils
+apt-get install -y sudo wget vim make zip unzip git debconf-utils
 
 # We now clone the ZPX software from GitHub
 echo "Downloading ZPanel, Please wait, this may take several minutes, the installer will continue after this is complete!"
